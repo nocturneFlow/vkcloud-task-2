1d5e6910987e99c6e6751a8d5c48b34b40170b5e
nocturneFlow <7evenxheaven@gmail.com>
2024-12-06 11:39:20 +0500

    Add initial components and utilities for the application

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,3 @@
+export default function AdminPage() {
+  return <div>some test.</div>;
+}
---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "@/components/forms/LoginForm";
+
+export default function SignIn() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <LoginForm />
+    </div>
+  );
+}
